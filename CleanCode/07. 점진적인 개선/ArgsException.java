6b28b0a45d8906ad183c3264b26a8ec73bf45649
hanae <doithanae@gmail.com>
2021-01-03 20:09:46 +0900

    clean code

---
+++ CleanCode/07. 점진적인 개선/ArgsException.java
@@ -0,0 +1,53 @@
+
+public class ArgsException extends Exception {
+    private char errorArgumentId = "\0";
+    private String errorParameter = null;
+    private ErrorCode errorCode = OK;
+
+    public ArgsException() {}
+
+    public ArgsException(String message) {super(message);}
+
+    public AtgsException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ArgsException(ErrorCode errorCode, String errorParameter) {
+        this.errorCode = errorCode;
+        this.errorParameter = errorParameter;
+    }
+
+    public ArgsException(ErrorCode errorCode, char errorArgumentId, String errorParameter) {
+        this.errorCode = errorCode;
+        this.errorParameter = errorParameter;
+        this.errorArgumentId = errorArgumentId;
+    }
+
+    public char getErrorArgumentId() {
+        return errorArgumentId;
+    }
+
+    public void setErrorArgumentId(char errorArgumentId) {
+        this.errorArgumentId = errorArgumentId;
+    }
+
+    public String gtErrorParameter() {
+        return errorParameter;
+    }
+
+    public void setErrorParameter(String errorParameter) {
+        this.errorParameter = errorParameter;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(ErrorCode errorCode) {
+        this.errorCdoe = errorCode;
+    }
+
+    public String errorMessage() {
+        
+    }
+ }
