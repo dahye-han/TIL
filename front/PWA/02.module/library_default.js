81437ddb14d743bd1289891016731213f62381e2
hanae <doithanae@gmail.com>
2021-01-23 21:25:59 +0900

    PWA 0123

---
+++ front/PWA/02.module/library_default.js
@@ -0,0 +1,2 @@
+const fnPlusNumbers = (Num1, Num2) => Num1 + Num2;
+export default fnPlusNumbers;
---
+++ front/PWA/02.module/library_named.js
@@ -0,0 +1,4 @@
+const conHello = '안녕하세요!';
+const fnPlusNumbers = (Num1, Num2) => Num1 + Num2;
+
+export { conHello, fnPlusNumbers };
