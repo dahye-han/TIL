14b46744d4b13bc82893962be00ef6ad3c5ad2ac
hanae <doithanae@gmail.com>
2021-01-30 22:33:36 +0900

    PWA 0130

--- front/PWA/todo/src/datasources/firebase.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase/app'
-import 'firebase/firebase-database'
-
-const oDB = firebase.initializeApp({
-    databaseURL: "https://demoapp-8eb67.firebaseio.com",
-}).database();
-
-export const oTodosinDB = oDB.ref('todos');
