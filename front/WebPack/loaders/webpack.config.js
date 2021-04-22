cdb7fa25fcd3abad0ab6d3dc81ac2c562c88f2dc
Hanae <doithanae@gmail.com>
2021-04-22 19:32:23 +0900

    output, loaders

---
+++ front/WebPack/loaders/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            // [style-loader](/loaders/style-loader)
+            { loader: 'style-loader' },
+            // [css-loader](/loaders/css-loader)
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true
+              }
+            },
+            // [sass-loader](/loaders/sass-loader)
+            { loader: 'sass-loader' }
+          ]
+        },
+        { test: /\.ts$/, use: 'ts-loader' }
+      ]
+    }
+  };
