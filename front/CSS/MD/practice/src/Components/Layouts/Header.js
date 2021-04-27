38013b0adc9b708ac7352b79a74189c7acac9ab7
Hanae <doithanae@gmail.com>
2021-04-27 22:14:26 +0900

    material design

--- front/CSS/MD/practice/src/Components/Layouts/Header.js
+++ front/CSS/MD/practice/src/Components/Layouts/Header.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 export default props =>
-    <div>
-        <h1>Header</h1>
-    </div>
+    <AppBar position="static">
+        <Toolbar>
+            <Typography variant="headline" color="inherit">
+                Exercise Database
+            </Typography>
+        </Toolbar>
+    </AppBar>   
