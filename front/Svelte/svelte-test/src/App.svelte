a5e7e927791593beced1b4eb9a101eb9a257ad73
Hanae <doithanae@gmail.com>
2021-05-17 20:54:56 +0900

    svelte

--- front/Svelte/svelte-test/src/App.svelte
+++ front/Svelte/svelte-test/src/App.svelte
@@ -1,30 +1,43 @@
 <script>
-	export let name;
+	let name = 'world';
+	let fruits = ['Apple', 'Banana', 'Cherry', 'Orange' , 'Mango'];
+	let age = 85;
+	function assign() {
+		name = 'HANAE'
+		age = 36
+	}
+	let toggle = false;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#if toggle}
+		<h1>Hello {name}!</h1>
+	{:else}
+		<h1>No name!</h1>
+	{/if}
+	<ul>
+		{#each fruits as fruit}
+			<li>{fruit}</li>
+		{/each}
+	</ul>
+	<h2 class={age > 85 ? "active" : ""}>
+		{age}
+	</h2>
+	<img src="" alt={name} />
+	<input type="text" bind:value={name} />
+	<button on:click={assign}>
+		Assign
+	</button>
+	<button on:click={() => {toggle = !toggle}}>
+		Toggle
+	</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		color : red;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.active {
+		color: blue;
 	}
 </style>
