c9a4b15d443023c4df6eae5289d5db2193fb6197
Hanae <doithanae@gmail.com>
2021-05-29 19:37:27 +0900

    python

---
+++ Python/practice6.py
@@ -0,0 +1,49 @@
+# 리스트 []
+
+# 지하철 칸별로 10명, 20명, 30명
+# subway1 = 10
+# subway2 = 20
+# subway3 = 30
+
+subway = [10, 20, 30]
+print(subway)
+
+subway = ["A", "B", "C"]
+print(subway)
+
+print(subway.index("B"))
+
+subway.append("D")
+print(subway)
+
+subway.insert(1, "E")
+print(subway)
+
+print(subway.pop())
+print(subway)
+
+subway.append("A")
+print(subway)
+print(subway.count("A"))
+
+# 정렬
+num_list = [5,2,4,3,1]
+num_list.sort()
+print(num_list)
+
+# 순서 뒤집기
+num_list.reverse()
+print(num_list)
+
+# 모두 지우기
+num_list.clear()
+print(num_list)
+
+# 다양한 자료형 함께 사용
+num_list = [5,2,4,3,1]
+mix_list = ["A", 20, True]
+# print(mix_list)
+
+# 리스트 확장
+num_list.extend(mix_list)
+print(num_list)
