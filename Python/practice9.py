bd8ee4307a784c8efe21357b98f1e84f79da01c2
Hanae <doithanae@gmail.com>
2021-06-02 22:42:08 +0900

    python

--- Python/practice9.py
+++ Python/practice9.py
@@ -1,3 +1,6 @@
+
+# if
+
 # weather = input("오늘 날씨는 어떄요? ")
 # if weather == "비" or weather == "눈":
 #     print("우산을 챙기세요.")
@@ -6,12 +9,21 @@
 # else:
 #     print("오늘은 준비물이 필요없어요.")
 
-temp = int(input("기온은 어때요? "))
-if 30 <= temp:
-    print("너무 더워요. 나가지 마세요.")
-elif 10 <= temp and temp < 30 :
-    print("괜찮은 날씨예요")
-elif 0 <= temp < 10:
-    print("외투를 챙기세요")
-else:
-    print("너무 추워요. 나가지 마세요")
+# temp = int(input("기온은 어때요? "))
+# if 30 <= temp:
+#     print("너무 더워요. 나가지 마세요.")
+# elif 10 <= temp and temp < 30 :
+#     print("괜찮은 날씨예요")
+# elif 0 <= temp < 10:
+#     print("외투를 챙기세요")
+# else:
+#     print("너무 추워요. 나가지 마세요")
+
+# for
+# print("대기번호 : 1")
+# print("대기번호 : 2")
+# print("대기번호 : 3")
+# print("대기번호 : 4")
+
+for waiting_no in range(1, 6):
+    print("대기번호 : {0}".format(waiting_no))
