affe8a1f2a0125ade6452b9235fe44562ad8460a
Hanae <doithanae@gmail.com>
2021-06-06 08:57:45 +0900

    python

--- Python/practice9.py
+++ Python/practice9.py
@@ -73,6 +73,18 @@
 # print(students)
 
 # 학생 이름을 대문자로 변환
-students = ["Iron man", "Thor", "I am groot"]
-students = [i.upper() for i in students]
-print(students)
+# students = ["Iron man", "Thor", "I am groot"]
+# students = [i.upper() for i in students]
+# print(students)
+
+from random import *
+cnt = 0 # 총 탑승 승객 수
+for i in range(1, 51): 
+    time = randrange(5, 51) 
+    if 5 <= time <= 15:
+        print("[0] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+        cnt += 1
+    else:
+        print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+
+print("총 탑습 승객 : {0}".format(cnt))
