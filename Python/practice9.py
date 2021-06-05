5244b6c7fd6cec00f9cd1885b37d145c896c2983
Hanae <doithanae@gmail.com>
2021-06-05 20:48:08 +0900

    python

--- Python/practice9.py
+++ Python/practice9.py
@@ -51,13 +51,28 @@
 #     person = input("이름이 어떻게 되세요?")
 
 # continue / break
-absent = [2, 5] # 결석
-no_book = [7] # 책을 깜빡했음
-for student in range(1, 11):
-    if student in absent:
-        continue
-    elif student in no_book:
-        print("오늘 수업 여기까지. {0}는 교무실로 따라와".format(student))
-        break
-    print("{0}, 책을 읽어봐".format(student))
+# absent = [2, 5] # 결석
+# no_book = [7] # 책을 깜빡했음
+# for student in range(1, 11):
+#     if student in absent:
+#         continue
+#     elif student in no_book:
+#         print("오늘 수업 여기까지. {0}는 교무실로 따라와".format(student))
+#         break
+#     print("{0}, 책을 읽어봐".format(student))
 
+# 출석번호가 1 2 3 4 앞에 100을 붙이기로 함
+# students = [1,2,3,4,5]
+# print(students)
+# students = [i+100 for i in students]
+# print(students)
+
+# 학생 이름을 길이로 변환
+# students = ["Iron man", "Thor", "I am groot"]
+# students = [len(i) for i in students]
+# print(students)
+
+# 학생 이름을 대문자로 변환
+students = ["Iron man", "Thor", "I am groot"]
+students = [i.upper() for i in students]
+print(students)
