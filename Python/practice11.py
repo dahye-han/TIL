5edb1a50c9777e1360158190dae24b9cebe461cf
Hanae <doithanae@gmail.com>
2021-06-10 20:27:23 +0900

    python

--- Python/practice11.py
+++ Python/practice11.py
@@ -1,3 +1,4 @@
+# 기본값
 # def profile(name, age, main_lang):
 #     print("이름 : {0}\t나이 : {1}\t주 사용 언어: {2}".format(name, age, main_lang))
 
@@ -5,8 +6,15 @@
 # profile("B", 25, "Java")
 
 # 같은 학교 같은 학년 같은 반 같은 수업.
-def profile(name, age=17, main_lang="Python"):
-    print("이름 : {0}\t나이 : {1}\t주 사용 언어: {2}".format(name, age, main_lang))
+# def profile(name, age=17, main_lang="Python"):
+#     print("이름 : {0}\t나이 : {1}\t주 사용 언어: {2}".format(name, age, main_lang))
 
-profile("A")
-profile("B")
+# profile("A")
+# profile("B")
+
+# 키워드값
+def profile(name, age, main_lang):
+    print(name, age, main_lang)
+
+profile(name="A", main_lang="python", age=20)
+profile(main_lang="java", age=25, name="B")
