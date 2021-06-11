5979e45606499d1f78045320b610efdf43d1c4fb
Hanae <doithanae@gmail.com>
2021-06-11 23:27:04 +0900

    python

--- Python/practice11.py
+++ Python/practice11.py
@@ -13,8 +13,22 @@
 # profile("B")
 
 # 키워드값
-def profile(name, age, main_lang):
-    print(name, age, main_lang)
+# def profile(name, age, main_lang):
+#     print(name, age, main_lang)
 
-profile(name="A", main_lang="python", age=20)
-profile(main_lang="java", age=25, name="B")
+# profile(name="A", main_lang="python", age=20)
+# profile(main_lang="java", age=25, name="B")
+
+# 가변인자
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+#     print(lang1, lang2, lang3, lang4, lang5)
+
+def profile(name, age, *language):
+    print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+    for lang in language:
+        print(lang, end=" ")
+    print()
+
+profile("A", 20, "python", "java", "C", "C++", "C#")
+profile("B", 25, "Kotlin", "Swift", "", "", "")
