5858201c70787c2fcfdcbd686371d89ec568c0c8
Hanae <doithanae@gmail.com>
2021-06-28 20:53:34 +0900

    python

---
+++ Python/practice20.py
@@ -0,0 +1,12 @@
+# import travel.thailand
+# # import travel.thailand.ThailandPackage() -> 사용 불가
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+# from travel.thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+from travel import vietnam
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
