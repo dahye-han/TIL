8c096b321c9deabbf9a4a19416f49ad5ce7f4638
Hanae <doithanae@gmail.com>
2021-07-11 21:56:25 +0900

    bs4

--- Python/webscraping/webscraping_basic/9_bs4_coupang.py
+++ Python/webscraping/webscraping_basic/9_bs4_coupang.py
@@ -11,17 +11,33 @@ soup = BeautifulSoup(res.text, "lxml")
 items = soup.find_all("li", attrs={"class":re.compile("^search-product")})
 # print(items[0].find("div", attrs={"class":"name"}).get_text())
 for item in items:
+
+    # 광고 상품 제외합니다.
+    ad_badge = item.find("span", attrs={"class":"ad-badge-text"})
+    if ad_badge:
+        print(" <광고 상품 제외합니다>")
+        continue
+
     name = item.find("div", attrs={"class":"name"}).get_text() # 제품명
+    if "Apple" in name:
+        print(" <Apple 상품 제외합니다>")
+        continue
+
     price = item.find("strong", attrs={"class":"price-value"}).get_text() # 가격
     rate = item.find("em", attrs={"class":"rating"}) # 평점 (rating)
     if rate:
         rate = rate.get_text()
     else:
-        rate = "평점 없음"
-    rate_count = item.find("span", attrs={"class":"rating-total-count"}) # 평점 수 (rating-total-count)
-    if rate_count:
-        rate_count = rate_count.get_text()
+        print(" <평점 없는 상품 제외합니다>")
+        continue
+
+    rate_cnt = item.find("span", attrs={"class":"rating-total-count"}) # 평점 수 (rating-total-count)
+    if rate_cnt:
+        rate_cnt = rate_cnt.get_text()
+        rate_cnt = rate_cnt[1:-1]
     else:
-        rate_count = "평점 없음"
+        print(" <평점 수 없는 상품 제외합니다>")
+        continue
     
-    print(name, price, rate, rate_count)
+    if float(rate) >= 4.5 and int(rate_cnt) >= 100:
+        print(name, price, rate, rate_cnt)
